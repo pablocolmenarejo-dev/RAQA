b1e0ccd3fd877ace3fd436f3db51c80a29ab4810
pablocolmenarejo-dev <pablo.colmenarejo@meisys.es>
2025-09-27 00:02:24 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+// src/main.tsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App"; // ¡ojo a la A mayúscula!
 
-const el = document.getElementById('root')!
+const el = document.getElementById("root")!;
 createRoot(el).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
